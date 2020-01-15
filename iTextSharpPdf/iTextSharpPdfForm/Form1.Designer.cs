10199bc9ebd39d1b6ff772bb5f030073b513c7fa
Kadir Yılmaz <kadirylmaz02@gmail.com>
2020-01-15 20:47:11 +0300

    Kaydetme

---
+++ iTextSharpPdf/iTextSharpPdfForm/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace iTextSharpPdfForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPdf = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPdf
+            // 
+            this.btnPdf.Location = new System.Drawing.Point(246, 105);
+            this.btnPdf.Name = "btnPdf";
+            this.btnPdf.Size = new System.Drawing.Size(75, 23);
+            this.btnPdf.TabIndex = 0;
+            this.btnPdf.Text = "Pdf Al";
+            this.btnPdf.UseVisualStyleBackColor = true;
+            this.btnPdf.Click += new System.EventHandler(this.btnPdf_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 319);
+            this.Controls.Add(this.btnPdf);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPdf;
+    }
+}
+
---
+++ iTextSharpPdf/iTextSharpPdfForm/Form1.cs
@@ -0,0 +1,69 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Web;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iTextSharpPdfForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPdf_Click(object sender, EventArgs e)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                Document document = new Document(PageSize.A4);
+                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(@"D:deneme.pdf",FileMode.Create));
+
+                document.Open();
+
+                BaseFont baseFont = BaseFont.CreateFont(@"C:\Windows\Fonts\arial.ttf", "windows-1254", BaseFont.EMBEDDED);
+                Font font = new Font(baseFont);
+
+                PdfPTable table = new PdfPTable(4); //5 sütunlu bir tablo ekliyoruz.
+
+                table.DefaultCell.Padding = 10;
+                table.TotalWidth = 500f;
+                table.LockedWidth = true;
+                PdfPCell cell = new PdfPCell(new Phrase("PDF"));
+
+                cell.Padding = 10;
+                cell.Colspan = 4;
+                cell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+
+                table.AddCell(new Phrase("1.Sütun", font));
+                table.AddCell(new Phrase("2.Sütun", font));
+                table.AddCell(new Phrase("3.Sütun", font));
+                table.AddCell(new Phrase("4.Sütun", font));
+
+
+                try
+                {
+                    document.Add(table);
+                    document.Close();
+                    writer.Close();
+                    MessageBox.Show("PDF Olusturuldu.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    throw;
+                }
+                    
+            }
+        }
+    }
+}
